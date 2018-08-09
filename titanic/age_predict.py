8bbc38d96bd1f828a43fbf3289f173041d527cf5
MLcodebase <chutianqianli@163.com>
2018-08-09 22:05:28 +0800

    fill age

--- titanic/age_predict.py
+++ titanic/age_predict.py
@@ -1,12 +1,12 @@
 import pandas as pd
 root_path = 'data'
-missing_age = pd.read_csv('%s/%s' % (root_path, 'pre_proc.csv'))
-missing_age = missing_age.drop(['Survived','Cabin','age_nan'],axis=1)
+train_test = pd.read_csv('%s/%s' % (root_path, 'pre_proc.csv'))
+missing_age = train_test.drop(['Survived','Cabin','age_nan'],axis=1)
 # process non numeric feature
 #missing_age = pd.get_dummies(missing_age,columns=['Sex','Embarked','fineTitle','Name2_new','Ticket_Letter'])
 #missing_age = missing_age.drop(['corseTitle'],axis=1)
-missing_age = pd.get_dummies(missing_age,columns=['Sex','Embarked','corseTitle','Ticket_Letter'])
-missing_age = missing_age.drop(['fineTitle','Name2_new'],axis=1)
+missing_age = pd.get_dummies(missing_age,columns=['Sex','Embarked','fineTitle','Ticket_Letter'])
+missing_age = missing_age.drop(['corseTitle','Name2_new'],axis=1)
 missing_age_train = missing_age[missing_age['Age'].notnull()]
 missing_age_test = missing_age[missing_age['Age'].isnull()]
 missing_age_X_train = missing_age_train.drop(['Age'], axis=1)
@@ -26,4 +26,8 @@ rf = RandomForestRegressor(random_state=5)
 rf_scores = cross_validation.cross_val_score(rf, missing_age_X_train, missing_age_Y_train, cv=5, scoring='mean_absolute_error')
 from sklearn.ensemble import GradientBoostingRegressor
 gbrt = GradientBoostingRegressor()
-rf_scores = cross_validation.cross_val_score(gbrt, missing_age_X_train, missing_age_Y_train, cv=5, scoring='mean_absolute_error')
+gbrt_scores = cross_validation.cross_val_score(gbrt, missing_age_X_train, missing_age_Y_train, cv=5, scoring='mean_absolute_error')
+gbrt.fit(missing_age_X_train,missing_age_Y_train)
+train_test.loc[(train_test['Age'].isnull()), 'Age'] = gbrt.predict(missing_age_X_test)
+train_test['corseAge'] = pd.cut(train_test['Age'], bins=[0,10,18,30,50,100],labels=[1,2,3,4,5])
+train_test.to_csv('%s/%s' % (root_path, 'fill_age.csv'),index=False)
