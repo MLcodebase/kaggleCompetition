5f767af22c16dbbdaefc8fd1ffd889a71335a4e9
MLcodebase <chutianqianli@163.com>
2018-08-15 20:42:33 +0800

    hyper param tune

--- titanic/age_predict_cls.py
+++ titanic/age_predict_cls.py
@@ -16,24 +16,37 @@ missing_age_X_train = ss.transform(missing_age_X_train)
 missing_age_X_test = ss.transform(missing_age_X_test)
 from sklearn import cross_validation
 missing_age_Y_train_class = pd.cut(missing_age_Y_train,bins=[0,12,20,35,60,100],labels=[1,2,3,4,5])
+
 from sklearn.ensemble import GradientBoostingClassifier
-#gbrt_c = GradientBoostingClassifier(learning_rate=0.1,max_depth=6,n_estimators=200,min_samples_leaf=2)
 gbrt_c = GradientBoostingClassifier()
-param = { "max_depth":[5,6,7],
-         "n_estimators":[50,75,100,120],"criterion":['friedman_mse','mae'],
-         "min_samples_leaf":[2,3,4]}
+param = {"criterion":["mae"],
+         "learning_rate":[0.4,0.5,0.6],
+         "n_estimators":[95,100,105],
+         "min_samples_leaf":[2,3,4],
+         "max_depth":[4, 5],
+         }
 from sklearn.grid_search import GridSearchCV
-clf = GridSearchCV(gbrt_c, param,cv=4, n_jobs=-1, verbose=1)
+clf = GridSearchCV(gbrt_c, param,cv=5, n_jobs=-1, verbose=1)
 clf.fit(missing_age_X_train,missing_age_Y_train_class)
 clf.best_params_
-gbrt_c = GradientBoostingClassifier(n_estimators=50,learning_rate=0.1,max_depth=6,min_samples_leaf=4)
+gbrt_c = GradientBoostingClassifier(n_estimators=100,learning_rate=0.6,max_depth=4,min_samples_leaf=3,criterion='mae')
 gbrt_c_scores = cross_validation.cross_val_score(gbrt_c, missing_age_X_train, missing_age_Y_train_class, cv=5)
-gbrt_c.fit(missing_age_X_train,missing_age_Y_train_class)
+gbrt_c_scores
 
 import xgboost as xgb
 xgb_model = xgb.XGBClassifier()
+param = {
+    "n_estimators":[95,100,105],
+    "learning_rate":[0.005,0.01,0.02],
+    "max_depth":[4,5]
+         }
+clf = GridSearchCV(xgb_model, param,cv=5, n_jobs=-1, verbose=1)
+clf.fit(missing_age_X_train,missing_age_Y_train_class)
+clf.best_params_
+#xgb_model = xgb.XGBClassifier(n_estimators=95,learning_rate=0.01,max_depth=4)
+xgb_model = xgb.XGBClassifier(n_estimators=100,learning_rate=0.075,max_depth=4)
 xg_c_scores = cross_validation.cross_val_score(xgb_model, missing_age_X_train, missing_age_Y_train_class, cv=5)
-xgb_model.fit(missing_age_X_train,missing_age_Y_train_class)
+xg_c_scores
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.grid_search import GridSearchCV
@@ -46,12 +59,39 @@ clf.best_params_
 #lr = LogisticRegression(C=0.1,max_iter=100)
 lr = LogisticRegression(C=0.05,max_iter=50)
 lr_scores = cross_validation.cross_val_score(lr, missing_age_X_train, missing_age_Y_train_class, cv=5)
-
-train_test.loc[(train_test['Age'].isnull()),  'corseAge'] = xgb_model.predict(missing_age_X_test)
-train_test.loc[(train_test['Age'].notnull()), 'corseAge'] = missing_age_Y_train_class
-train_test.to_csv('%s/%s' % (root_path, 'fill_age-cls.csv'),index=False)
+lr_scores
 
 # use classification instead of regression
 from sklearn.ensemble import RandomForestClassifier
-rf_c = RandomForestClassifier(random_state=5)
+rf_c = RandomForestClassifier()
+param = {'random_state':[5,6,8,10,11,12,13,14,15],
+         'n_estimators':[40,50],
+         'min_samples_leaf': [2],
+         'max_depth': [8,9]}
+from sklearn.grid_search import GridSearchCV
+clf = GridSearchCV(rf_c, param,cv=5, n_jobs=-1, verbose=1)
+clf.fit(missing_age_X_train,missing_age_Y_train_class)
+clf.best_params_
+#rf_c = RandomForestClassifier(oob_score=True,min_samples_leaf=2,n_estimators=40,random_state=10,max_depth=8)/
+rf_c = RandomForestClassifier(oob_score=True,min_samples_leaf=2,n_estimators=50,random_state=13,max_depth=9)
 rf_c_scores = cross_validation.cross_val_score(rf_c, missing_age_X_train, missing_age_Y_train_class, cv=5)
+rf_c_scores
+
+from sklearn.ensemble import VotingClassifier
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression(C=0.05,max_iter=50)
+import xgboost as xgb
+xgb_model = xgb.XGBClassifier(n_estimators=100,learning_rate=0.075,max_depth=4)
+from sklearn.ensemble import GradientBoostingClassifier
+gbdt = GradientBoostingClassifier(learning_rate=0.6,max_depth=4,n_estimators=100,min_samples_leaf=3,criterion='mae')
+from sklearn.ensemble import RandomForestClassifier
+rf = RandomForestClassifier(oob_score=True,min_samples_leaf=2,n_estimators=50,random_state=13,max_depth=9)
+vot = VotingClassifier(estimators=[('lr', lr),('gbdt',gbdt),('xgb',xgb_model),('rf',rf)], voting='hard',n_jobs=-1)
+from sklearn import cross_validation
+vot_scores = cross_validation.cross_val_score(vot, missing_age_X_train, missing_age_Y_train_class, cv=5)
+vot_scores
+vot.fit(missing_age_X_train,missing_age_Y_train_class)
+
+train_test.loc[(train_test['Age'].isnull()),  'corseAge'] = vot.predict(missing_age_X_test)
+train_test.loc[(train_test['Age'].notnull()), 'corseAge'] = missing_age_Y_train_class
+train_test.to_csv('%s/%s' % (root_path, 'fill_age-cls.csv'),index=False)
--- titanic/model.py
+++ titanic/model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 train_test = pd.read_csv('data/fill_age-cls.csv')
-train_test = train_test.drop(['corseTitle'],axis=1)
-train_test = pd.get_dummies(train_test,columns=['Cabin','Sex','fineTitle','Ticket_Letter','Embarked','Name2_new','Age','Ticket_Num'])
+train_test = train_test.drop(['corseTitle','Age'],axis=1)
+train_test = pd.get_dummies(train_test,columns=['Cabin','Sex','fineTitle','Ticket_Letter','Embarked','Name2_new','corseAge','Ticket_Num'])
 train_data = train_test[:891]
 test_data = train_test[891:]
 train_data_X = train_data.drop(['Survived'],axis=1)
@@ -13,54 +13,89 @@ ss.fit(train_data_X)
 train_data_X_sd = ss.transform(train_data_X)
 test_data_X_sd  = ss.transform(test_data_X)
 
-from sklearn.ensemble import RandomForestClassifier
-#rf = RandomForestClassifier(n_estimators=150,min_samples_leaf=3,max_depth=6,oob_score=True)
-rf = RandomForestClassifier()
-from sklearn import cross_validation
-rf_scores = cross_validation.cross_val_score(rf, train_data_X, train_data_Y, cv=5)
-rf.fit(train_data_X,train_data_Y)
-
 from sklearn.linear_model import LogisticRegression
 from sklearn.grid_search import GridSearchCV
 lr = LogisticRegression()
-param = {'C':[0.001,0.01,0.1,1,10], "max_iter":[100,250]}
+param = {'C':[0.00125,0.015,0.02], "max_iter":[20,25,30,35,40,50]}
 clf = GridSearchCV(lr, param,cv=5, n_jobs=-1, verbose=1, scoring="roc_auc")
 clf.fit(train_data_X_sd, train_data_Y)
-clf.grid_scores_
 clf.best_params_
-lr_scores = cross_validation.cross_val_score(clf, train_data_X, train_data_Y, cv=5)
-lr = LogisticRegression(C=0.01,max_iter=100)
-lr.fit(train_data_X_sd, train_data_Y)
+from sklearn import cross_validation
+lr = LogisticRegression(C=0.015,max_iter=20)
+lr_scores = cross_validation.cross_val_score(lr, train_data_X, train_data_Y, cv=5)
+lr_scores
+
+from sklearn.ensemble import RandomForestClassifier
+rf = RandomForestClassifier()
+param = {'random_state':[1,2,3,4,5,6,7,8,9,10,11,12],
+         'n_estimators':[25,28,30,32],
+         'min_samples_leaf':[2],
+         'max_depth':[6,7,8],
+         'oob_score':[True]}
+clf = GridSearchCV(rf, param,cv=5, n_jobs=-1, verbose=1, scoring="roc_auc")
+clf.fit(train_data_X_sd, train_data_Y)
+clf.best_params_
+from sklearn import cross_validation
+rf = RandomForestClassifier(random_state=5,n_estimators=25,min_samples_leaf=2,max_depth=7,oob_score=True)
+rf_scores = cross_validation.cross_val_score(rf, train_data_X, train_data_Y, cv=5)
+rf_scores
 
 from sklearn.ensemble import GradientBoostingClassifier
 gbdt = GradientBoostingClassifier()
-#gbdt = GradientBoostingClassifier(learning_rate=0.7,max_depth=6,n_estimators=100,min_samples_leaf=2)
+param = {"criterion":["mae","friedman_mse"],
+         "learning_rate":[0.09,0.1,1.1],
+         "n_estimators":[95,100,105],
+         "min_samples_leaf":[2],
+         "max_depth":[4,5,6],
+         }
+from sklearn.grid_search import GridSearchCV
+clf = GridSearchCV(gbdt, param,cv=5, n_jobs=-1, verbose=1)
+clf.fit(train_data_X,train_data_Y)
+clf.best_params_
+
+gbdt = GradientBoostingClassifier(learning_rate=0.1,max_depth=4,n_estimators=105,min_samples_leaf=2)
 gbdt_scores = cross_validation.cross_val_score(gbdt, train_data_X, train_data_Y, cv=5)
-gbdt.fit(train_data_X,train_data_Y)
+gbdt_scores
 
 import xgboost as xgb
-#xgb_model = xgb.XGBClassifier(n_estimators=150,min_samples_leaf=3,max_depth=6)
 xgb_model = xgb.XGBClassifier()
+param = {
+    "n_estimators":[120,130,140,150],
+    "learning_rate":[0.045,0.05,0.055],
+    "max_depth":[6,7,8]
+         }
+from sklearn.grid_search import GridSearchCV
+clf = GridSearchCV(xgb_model, param,cv=5, n_jobs=-1, verbose=1)
+clf.fit(train_data_X,train_data_Y)
+clf.best_params_
+xgb_model = xgb.XGBClassifier(learning_rate=0.045,n_estimators=150,max_depth=7)
+from sklearn import cross_validation
 xg_scores = cross_validation.cross_val_score(xgb_model, train_data_X, train_data_Y, cv=5)
-xgb_model.fit(train_data_X,train_data_Y)
+xg_scores
 
 from sklearn.ensemble import VotingClassifier
 from sklearn.linear_model import LogisticRegression
-lr = LogisticRegression(C=0.1,max_iter=100)
-import xgboost as xgb
-xgb_model = xgb.XGBClassifier(n_estimators=150,min_samples_leaf=3,max_depth=6)
+lr = LogisticRegression(C=0.015,max_iter=20)
 from sklearn.ensemble import RandomForestClassifier
-rf = RandomForestClassifier(n_estimators=200,min_samples_leaf=2,max_depth=6,oob_score=True)
+rf = RandomForestClassifier(random_state=5,n_estimators=25,min_samples_leaf=2,max_depth=7,oob_score=True)
 from sklearn.ensemble import GradientBoostingClassifier
-gbdt = GradientBoostingClassifier(learning_rate=0.7,max_depth=6,n_estimators=100,min_samples_leaf=2)
+gbdt = GradientBoostingClassifier(learning_rate=0.1,max_depth=4,n_estimators=105,min_samples_leaf=2)
+import xgboost as xgb
+xgb_model = xgb.XGBClassifier(learning_rate=0.045,n_estimators=150,max_depth=7)
 vot = VotingClassifier(estimators=[('lr', lr), ('rf', rf),('gbdt',gbdt),('xgb',xgb_model)], voting='hard')
 from sklearn import cross_validation
 vot_scores = cross_validation.cross_val_score(vot, train_data_X, train_data_Y, cv=5)
-vot.fit(train_data_X_sd,train_data_Y)
 
+vot.fit(train_data_X_sd,train_data_Y)
 test = pd.read_csv('%s/%s' % ('data/', 'test.csv'))
-#test["Survived_f"] = vot.predict(test_data_X_sd)
 test["Survived_f"] = vot.predict(test_data_X_sd)
 test['Survived'] = test["Survived_f"].astype(int)
 test.to_csv('data/vot.csv',index=False)
-test[['PassengerId','Survived']].set_index('PassengerId').to_csv('data/sub6.csv')
+test[['PassengerId','Survived']].set_index('PassengerId').to_csv('data/sub7.csv')
+
+gbdt.fit(train_data_X_sd,train_data_Y)
+test = pd.read_csv('%s/%s' % ('data/', 'test.csv'))
+test["Survived_f"] = gbdt.predict(test_data_X_sd)
+test['Survived'] = test["Survived_f"].astype(int)
+test.to_csv('data/vot.csv',index=False)
+test[['PassengerId','Survived']].set_index('PassengerId').to_csv('data/sub8.csv')
